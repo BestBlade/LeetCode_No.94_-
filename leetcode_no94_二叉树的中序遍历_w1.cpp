25f4e987566920ba6cd677469cc27c60090214dd
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-08 11:23:09 +0800

    Add files via upload

---
+++ leetcode_no94_二叉树的中序遍历_w1.cpp
@@ -0,0 +1,89 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+ 
+
+示例 1：
+
+
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+示例 2：
+
+输入：root = []
+输出：[]
+示例 3：
+
+输入：root = [1]
+输出：[1]
+示例 4：
+
+
+输入：root = [1,2]
+输出：[2,1]
+示例 5：
+
+
+输入：root = [1,null,2]
+输出：[1,2]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 100] 内
+-100 <= Node.val <= 100
+ 
+
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	递归法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.8 MB, 在所有 C++ 提交中击败了8.12%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void dfs(TreeNode* root, vector<int>& res) {
+    if (!root) {
+        return;
+    }
+    dfs(root->left, res);
+    res.push_back(root->val);
+    dfs(root->right, res);
+    return;
+}
+
+//中序遍历：先左节点，再根节点，再右节点
+vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> res;
+    dfs(root, res);
+    return res;
+}
